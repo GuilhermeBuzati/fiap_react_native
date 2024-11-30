334b7656df8936793c860d059334b2b9a0a0df68
Guilherme Buzati <guilhermeleopoldinoo@gmail.com>
2024-11-30 02:05:25 -0300

    feat: Adicionado view de postagem

--- app/screens/Post/postItem.tsx
+++ app/screens/Post/postItem.tsx
@@ -1,52 +1,49 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import  Post  from '@/app/types/Post';  // Importando a tipagem do Post
 
-export default function PostItem({ route }: any) {
-  const { post } = route.params;
-  const navigation = useNavigation();
+
+export default function PostItem({ route, navigation }: { route: any, navigation: NavigationProp<any> }) {
+  const  {post} = route.params; 
 
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.title}>{post.title}</Text>
-      <Text style={styles.author}>Autor: {post.author}</Text>
+      <Text style={styles.author}>{post.author.name}</Text>
+      <Text style={styles.publishedAt}>Publicado em: {new Date(post.publishedAt).toLocaleDateString()}</Text>
       <Text style={styles.content}>{post.content}</Text>
-      <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
-        <Text style={styles.backButtonText}>Voltar</Text>
+
+      <TouchableOpacity style={styles.buttonBack} onPress={() => navigation.goBack()}>
+        <Text style={styles.buttonText}>Voltar</Text>
       </TouchableOpacity>
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
     padding: 20,
     backgroundColor: '#f9f9f9',
   },
-  backButton: {
-    backgroundColor: '#007BFF',
-    padding: 15,
-    borderRadius: 8,
-    alignItems: 'center',
-    marginTop: 10,
-  },
-  backButtonText: {
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
   title: {
-    fontSize: 28,
+    fontSize: 30,
     fontWeight: 'bold',
     color: '#333333',
-    marginBottom: 15,
+    marginBottom: 10,
     textAlign: 'center',
   },
   author: {
-    fontSize: 16,
+    fontSize: 18,
     color: '#555555',
     fontStyle: 'italic',
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  publishedAt: {
+    fontSize: 16,
+    color: '#777777',
     marginBottom: 20,
     textAlign: 'center',
   },
@@ -55,6 +52,18 @@ const styles = StyleSheet.create({
     lineHeight: 28,
     color: '#444444',
     textAlign: 'justify',
-    paddingHorizontal: 10,
+    marginBottom: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  buttonBack: {
+    backgroundColor: '#ccc',
+    padding: 15,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginTop: 10,
   },
 });
